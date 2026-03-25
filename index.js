e6efef95a115cc1d2be9c959e427034a52c78e21
Thierry29112004 <thierry.mariano@alunos.unis.edu.br>
2026-03-25 08:58:38 -0300

    Otimizando memória do Chrome para evitar travamentos no Render

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ let qrCodeAtual = '';
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage', /* Esse é o mágico que salva a memória no Render */
+            '--disable-gpu',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote'
+        ]
     }
 });
 
